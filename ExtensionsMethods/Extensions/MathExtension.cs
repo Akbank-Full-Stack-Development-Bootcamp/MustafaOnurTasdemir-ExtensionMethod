766a4f1d4ce0635aabc111846e8a9bde32ce7e84
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-06-06 21:50:32 +0300

    fixed

--- ExtensionsMethods/Extensions/MathExtension.cs
+++ ExtensionsMethods/Extensions/MathExtension.cs
@@ -9,42 +9,42 @@ namespace ExtensionsMethods.Extensions
     public static class MathExtension
     {
 
-        //public static string RecommendedUsername(this string Name, string userName)
-        //{
-        //    List<User> user = new List<User>()
-        //    {
-        //        new User(){userName="mustafa" },
-        //        new User(){userName="mustafa1" },
-        //        new User(){userName="mustafa2" }, //Örneğin veritabanımızda kayıtlı bulunan userName'ler
-        //        new User(){userName="mustafa3" },
-        //        new User(){userName="mustafa4" }
+        public static string RecommendedUsername(this string Name, string userName)
+        {
+            List<User> user = new List<User>()
+            {
+                new User(){userName="mustafa" },
+                new User(){userName="mustafa1" },
+                new User(){userName="mustafa2" }, //Örneğin veritabanımızda kayıtlı bulunan userName'ler
+                new User(){userName="mustafa3" },
+                new User(){userName="mustafa4" }
 
-        //    };
-        //    string name = userName;
-        //    Random random = new Random();
-        //    foreach (var item in user)
-        //    {
-        //        // Kullanıcı tarafından girilen userName ve DB'de bulunan userName'ler karşılaştırılıyor ve aynısı bulunuyorsa alternatif bir userName oluşturuyor.
-        //        //bulunmuyor ise Db'ye kaydediyor.
-        //        if (name == item.userName)
-        //        {
-        //            string randomNumber = random.Next(1, 10).ToString();
-        //            name = name + randomNumber;
-        //        }
+            };
+            string name = userName;
+            Random random = new Random();
+            foreach (var item in user)
+            {
+                // Kullanıcı tarafından girilen userName ve DB'de bulunan userName'ler karşılaştırılıyor ve aynısı bulunuyorsa alternatif bir userName oluşturuyor.
+                //bulunmuyor ise Db'ye kaydediyor.
+                if (name == item.userName)
+                {
+                    string randomNumber = random.Next(1, 10).ToString();
+                    name = name + randomNumber;
+                }
 
-        //    }
-        //    Console.Write("Veri Tabanında Bulunan Kullancı Adları: ");
-        //    foreach (var item in user)
-        //    {
-        //        Console.Write(item.userName + " ");
-        //    }
-        //    user.Add(new User()
-        //    {
-        //        userName = name
-        //    });
+            }
+            Console.Write("Veri Tabanında Bulunan Kullancı Adları: ");
+            foreach (var item in user)
+            {
+                Console.Write(item.userName + " ");
+            }
+            user.Add(new User()
+            {
+                userName = name
+            });
 
-        //    return name;
-        //}
+            return name;
+        }
 
 
         public static string encryptionMethod(this string Text, string text, int encryption)
@@ -72,7 +72,7 @@ namespace ExtensionsMethods.Extensions
         }
         public static string decryptionMethod(this string text, string encryptedtext, int encryption)
         {
-            int a;
+            int a=0;
             int calc;
 
             string decryptiontext = "";
@@ -86,12 +86,12 @@ namespace ExtensionsMethods.Extensions
                 }
                 else if (char.IsUpper(character))
                 {
-                    calc = a.AsciiCalculate(character, 65 - encryption);
+                    calc = a.AsciiCalculate(character, 65 , encryption);
                     decryptiontext += Convert.ToChar((calc % 26) + 65).ToString();
                 }
                 else
                 {
-                    calc = a.AsciiCalculate(character, 97 - encryption);
+                    calc = a.AsciiCalculate(character, 97,encryption);
                     decryptiontext += Convert.ToChar((calc % 26) + 97).ToString();
                 }
             }
--- ExtensionsMethods/Program.cs
+++ ExtensionsMethods/Program.cs
@@ -7,30 +7,31 @@ namespace ExtensionsMethods
 {
     class Program
     {
-        private static string username;
+        private static string userName;
 
         static void Main(string[] args)
         {
 
             //Önerilen kullanıcı adı : Kullanıcı tarafından girilen ama örneğin veritabanında kayıtlı olan userName'e alternatif olarak bir öneride bulunmak.
 
+            Console.WriteLine("*******Kullanıcı Adı Önerme******");
             Console.Write("Kullanıcı Tarafından Girilen Kullanıcı Adı: ");
             string userName = Console.ReadLine();
             string availableUsername = userName.RecommendedUsername(userName);
             Console.WriteLine(" ");
             Console.WriteLine("Önerilen Kullanıcı Adı: " + availableUsername);
 
+            Console.WriteLine("");
 
-
-
+            Console.WriteLine("**********Sezar Şifreleme Yöntemi*******");
             // Sezar Şifreleme : kullanıcı tarafından girilen metin ve anahtar vardır. Bu anahtara göre metin şifrelenir.
             //Anahtar değeri kadar metinde bulunan harfler ötelenir. Örneğin anahtar değerimiz 2, 'a' harfi 'c' olacaktır.
 
             Console.Write("lütfen türkçe karakter olmayan bir metin giriniz: ");
-            string text = Console.Readline();
+            string text = Console.ReadLine();
 
             Console.Write("şifreleme yöntemi: ");
-            int encryption = Convert.ToInt32(Console.Readline());
+            int encryption = Convert.ToInt32(Console.ReadLine());
 
             string encryptedtext = text.encryptionMethod(text, encryption);
             Console.WriteLine("sezar şifrelemesi uygulanmış metin:" + encryptedtext);
