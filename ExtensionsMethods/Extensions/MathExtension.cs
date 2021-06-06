1a85faae79ebff10d14707b76fdbc92ebe6b59dc
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-06-06 21:36:01 +0300

    Add project files.

---
+++ ExtensionsMethods/Extensions/MathExtension.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsMethods.Extensions
+{
+    public static class MathExtension
+    {
+
+        //public static string RecommendedUsername(this string Name, string userName)
+        //{
+        //    List<User> user = new List<User>()
+        //    {
+        //        new User(){userName="mustafa" },
+        //        new User(){userName="mustafa1" },
+        //        new User(){userName="mustafa2" }, //Örneğin veritabanımızda kayıtlı bulunan userName'ler
+        //        new User(){userName="mustafa3" },
+        //        new User(){userName="mustafa4" }
+
+        //    };
+        //    string name = userName;
+        //    Random random = new Random();
+        //    foreach (var item in user)
+        //    {
+        //        // Kullanıcı tarafından girilen userName ve DB'de bulunan userName'ler karşılaştırılıyor ve aynısı bulunuyorsa alternatif bir userName oluşturuyor.
+        //        //bulunmuyor ise Db'ye kaydediyor.
+        //        if (name == item.userName)
+        //        {
+        //            string randomNumber = random.Next(1, 10).ToString();
+        //            name = name + randomNumber;
+        //        }
+
+        //    }
+        //    Console.Write("Veri Tabanında Bulunan Kullancı Adları: ");
+        //    foreach (var item in user)
+        //    {
+        //        Console.Write(item.userName + " ");
+        //    }
+        //    user.Add(new User()
+        //    {
+        //        userName = name
+        //    });
+
+        //    return name;
+        //}
+
+
+        public static string encryptionMethod(this string Text, string text, int encryption)
+        {
+            string encryptionText = "";
+            char[] characters = text.ToCharArray();  
+            foreach (char character in characters)   
+            {
+                if (character == ' ')
+                {
+                    encryptionText += " ";          
+                    continue;
+                }
+                else if (Char.IsUpper(character))
+                {
+                    encryptionText += Convert.ToChar(((character - 65 + encryption) % 26 + 65)).ToString();
+                }
+                else
+                {
+                    encryptionText += Convert.ToChar(((character - 97 + encryption) % 26 + 97)).ToString();
+                }
+
+            }
+            return encryptionText;
+        }
+        public static string decryptionMethod(this string text, string encryptedtext, int encryption)
+        {
+            int a;
+            int calc;
+
+            string decryptiontext = "";
+            char[] characters = encryptedtext.ToCharArray();
+            foreach (var character in characters)
+            {
+                if (character == ' ')
+                {
+                    decryptiontext += " ";
+                    continue;
+                }
+                else if (char.IsUpper(character))
+                {
+                    calc = a.AsciiCalculate(character, 65 - encryption);
+                    decryptiontext += Convert.ToChar((calc % 26) + 65).ToString();
+                }
+                else
+                {
+                    calc = a.AsciiCalculate(character, 97 - encryption);
+                    decryptiontext += Convert.ToChar((calc % 26) + 97).ToString();
+                }
+            }
+            return decryptiontext;
+
+        }
+        public static int AsciiCalculate(this int Calc, char character, int c, int encryption)
+        {
+            int asciiCalculate = (character - c - encryption);
+            if (asciiCalculate < 0)
+            {
+                asciiCalculate += 26;
+            }
+            return asciiCalculate;
+        }
+
+    }
+}
---
+++ ExtensionsMethods/Program.cs
@@ -0,0 +1,45 @@
+﻿using ExtensionsMethods.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionsMethods
+{
+    class Program
+    {
+        private static string username;
+
+        static void Main(string[] args)
+        {
+
+            //Önerilen kullanıcı adı : Kullanıcı tarafından girilen ama örneğin veritabanında kayıtlı olan userName'e alternatif olarak bir öneride bulunmak.
+
+            Console.Write("Kullanıcı Tarafından Girilen Kullanıcı Adı: ");
+            string userName = Console.ReadLine();
+            string availableUsername = userName.RecommendedUsername(userName);
+            Console.WriteLine(" ");
+            Console.WriteLine("Önerilen Kullanıcı Adı: " + availableUsername);
+
+
+
+
+            // Sezar Şifreleme : kullanıcı tarafından girilen metin ve anahtar vardır. Bu anahtara göre metin şifrelenir.
+            //Anahtar değeri kadar metinde bulunan harfler ötelenir. Örneğin anahtar değerimiz 2, 'a' harfi 'c' olacaktır.
+
+            Console.Write("lütfen türkçe karakter olmayan bir metin giriniz: ");
+            string text = Console.Readline();
+
+            Console.Write("şifreleme yöntemi: ");
+            int encryption = Convert.ToInt32(Console.Readline());
+
+            string encryptedtext = text.encryptionMethod(text, encryption);
+            Console.WriteLine("sezar şifrelemesi uygulanmış metin:" + encryptedtext);
+
+            string decryptedtext = text.decryptionMethod(encryptedtext, encryption);
+            Console.WriteLine("şifrelenmiş metnin çözülmüş hali:" + decryptedtext);
+
+
+        }
+    }
+    
+}
